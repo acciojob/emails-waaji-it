35b2a4888208282e725087ddaee5ad2c5210df27
waaji-it <abdul.wajid.sahib@gmail.com>
2023-02-25 14:31:17 +0530

    First attempt

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,31 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword==password){
+            if(newPassword.length()<8)
+                return;
+            boolean upperCase = false;
+            boolean lowerCase = false;
+            boolean digit = false;
+            boolean specialChar = false;
+            for(int i=0 ; i<newPassword.length();i++){
+                if(newPassword.charAt(i)>='A' && newPassword.charAt(i)<='Z'){
+                    upperCase = true;
+                }
+                if(newPassword.charAt(i)>='a' && newPassword.charAt(i)<='z'){
+                    lowerCase = true;
+                }
+                if(newPassword.charAt(i)>='0' && newPassword.charAt(i)<='9')
+                    digit = true;
+                if(!(newPassword.charAt(i)>='A' && newPassword.charAt(i)<='Z') && !(newPassword.charAt(i)>='a' && newPassword.charAt(i)<='z') && !(newPassword.charAt(i)>='0' && newPassword.charAt(i)<='9'))
+                    specialChar = true;
+            }
+            if(!upperCase) return;
+            if(!lowerCase) return;
+            if(!digit) return;
+            if(!specialChar) return;
+            this.password = newPassword;
+        }
     }
+
 }
